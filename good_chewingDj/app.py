0f72572c95ff39a010b512a86212f826a6cf3443
SinicaSusu <q7s5w8a4@gmail.com>
2022-08-19 13:29:50 +0800

    init

--- good_chewingDj/app.py
+++ good_chewingDj/app.py
@@ -2,6 +2,9 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
+from pydub import AudioSegment
+import speech_recognition as sr
+
 import os
 
 app = Flask(__name__)
@@ -25,6 +28,33 @@ def callback():
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
+
+@handler.add(MessageEvent, message=AudioMessage)  # 取得聲音時做的事情
+def handle_message_Audio(event):
+    #接收使用者語音訊息並存檔
+    UserID = event.source.user_id
+    path="./audio/"+UserID+".wav"
+    audio_content = line_bot_api.get_message_content(event.message.id)
+    with open(path, 'wb') as fd:
+        for chunk in audio_content.iter_content():
+            fd.write(chunk)        
+    fd.close()
+    
+    #轉檔
+    AudioSegment.converter = './ffmpeg/bin/ffmpeg.exe'
+    sound = AudioSegment.from_file_using_temporary_files(path)
+    path = os.path.splitext(path)[0]+'.wav'
+    sound.export(path, format="wav")
+    
+    #辨識
+    r = sr.Recognizer()
+    with sr.AudioFile(path) as source:
+        audio = r.record(source)
+    text = r.recognize_google(audio,language='zh-Hant')
+    
+    #回傳訊息給使用者
+    event.message.text=text
+    handle_message(event)
 
 import os
 if __name__ == "__main__":
